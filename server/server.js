0c2f7dc4e494fe378e9f7a9caf332f5e7f82a255
aprajita0 <aprajita0625@gmail.com>
2025-08-03 15:28:17 -0400

    Connected to Mongo and started the server.

---
+++ server/server.js
@@ -0,0 +1,31 @@
+const dotenv = require('dotenv');
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+dotenv.config();
+const app = express();
+
+app.use(express.json());
+app.use(cors(
+    {
+        origin: '*',
+        credentials: true,
+        allowedHeaders: ['Content-Type', 'Authorization']
+    }
+));
+
+const PORT = 4000;
+
+const uri = process.env.Mongo_URI;
+
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('✅ Connected to MongoDB');
+    app.listen(PORT, () => {
+      console.log(`🚀 Server is running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error('❌ MongoDB connection failed:', error);
+  });
